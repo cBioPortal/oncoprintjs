41c3a18097fd3d97cdad80fa7e486dbc840c1b6a
Gideon Dresdner <gideond@gmail.com>
2015-04-05 09:14:35 +0300

    step 1: expose something
    trying to expose a basic function that calls alert.

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var renderers = require('./renderers');
+var engine = require('./rendering_engine');
+var utils = require('./utils');
+
+var exports = module.exports = {};
+
+exports.oncoprint = function oncoprint() {
+  alert("HELLO WORLD");
+}
