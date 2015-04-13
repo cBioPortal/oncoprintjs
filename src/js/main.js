d3f7692be20d17a96a60324d0617513d1b04483e
Gideon Dresdner <gideond@gmail.com>
2015-04-13 18:35:19 +0300

    compiling prod code with minifcation.
    now for figuring out what i actually want to expose.

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,14 @@ var utils = require('./utils');
 
 var exports = module.exports = {};
 
+var aSetting = "HELLO WORLD";
+
 exports.oncoprint = function oncoprint() {
   alert("HELLO WORLD");
+};
+
+exports.aSetting = function(value) {
+  if (!arguments.length) return aSetting;
+  aSetting = value;
+  return exports;
 };
