21f994f22000354d330ae880725c116baf9daec9
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-11-03 10:52:25 -0400

    Webpack and webworkers and clustering
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+module.exports = {
+    entry: './src/js/oncoprint.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'oncoprint.bundle.js',
+        library: 'oncoprintjs',
+        libraryTarget: 'commonjs-module'
+    },
+    plugins: [
+        new WebpackShellPlugin({onBuildStart:['touch dist', 'rm -rf dist', 'mkdir dist']})
+    ]
+};
